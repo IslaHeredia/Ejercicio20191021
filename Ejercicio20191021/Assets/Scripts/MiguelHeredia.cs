a41d481dfa6f44c3fb06cdfbb7876ab41f61a847
IslaHeredia <miguelheredia@ceitartessos.com>
2019-10-21 19:17:44 +0200

    ejercicio Mi Nombre

---
+++ Ejercicio20191021/Assets/Scripts/MiguelHeredia.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiguelHeredia : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		Debug.Log ("Miguel Heredia, AKA IslaHeredia");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("treintaypocos");
+	}
+}
